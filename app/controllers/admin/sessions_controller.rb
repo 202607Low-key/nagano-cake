e7bb73db4807ce0a371a625f968e4d2688741894
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-08-13 16:27:27 +0000

    Add admin/login

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,5 +1,5 @@
-class Admin::SessionsController < ApplicationController
-  allow_unauthenticated_access only: %i[ new create]
+class Admin::SessionsController < Admin::ApplicationController
+  allow_unauthenticated_access only: %i[new create]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_admin_session_url, alert: "Try again later." }
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
-  devise_for :users
+  devise_for :customers
+
+  # 管理者用
+  namespace :admin do
+    get "homes/top"
+    resource :session, only: [:new, :create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
