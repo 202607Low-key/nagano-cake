66557f2301841e5fb36ad06ab11900569689860a
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-08-14 17:06:15 +0000

    registrations_controller修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Public::RegistrationsController < Public::ApplicationController
-  allow_unauthenticated_access only %i[ new create ]
+  allow_unauthenticated_access only: %i[ new create ]
 
   def new
     @customer = Customer.new
