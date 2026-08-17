37f332a3c847190979d8b1ed955773a20851fb3e
Ubuntu <ubuntu@ip-172-31-43-161.ap-northeast-1.compute.internal>
2026-08-17 15:30:24 +0000

    add public/addresses controller and views

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,40 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Current.customer.addresses
   end
 
   def edit
+    @address = Current.customer.addresses.find(params[:id])
   end
 
   def create
+    @address = Current.customer.addresses.new(address_params)
+    if @address.save
+      redirect_to addresses_path
+    else
+      render :index
+    end
   end
 
   def update
+    @address = Current.customer.addresses.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @address = Current.customer.addresses.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
   end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+  
 end
