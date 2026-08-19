8bd09574042e7a06d85913bc5b1422269a991556
Ubuntu <ubuntu@ip-172-31-43-161.ap-northeast-1.compute.internal>
2026-08-19 02:48:20 +0000

    implement public/items index and show

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,10 @@
 class Public::ItemsController < ApplicationController
+  allow_unauthenticated_access only: [:index, :show]
   def index
+    @items = Item.includes(:genre).page(params[:page]).where(is_active: true)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
