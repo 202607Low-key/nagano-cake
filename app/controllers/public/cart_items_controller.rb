5005c4e4aa99ba69b57bb20fd1cce20dd02d1e39
Ubuntu <ubuntu@ip-172-31-43-161.ap-northeast-1.compute.internal>
2026-08-20 11:01:47 +0000

    implement cart_items create, index with subtotal and total price

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,20 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = Current.customer.cart_items
+    @total_price = 0
   end
 
   def create
+    @cart_item = Current.customer.cart_items.new(cart_item_params)
+    existing_cart_item = Current.customer.cart_items.find_by(item_id: cart_item_params[:item_id])
+
+    if existing_cart_item
+      existing_cart_item.update(amount: existing_cart_item.amount + cart_item_params[:amount])
+    else
+      @cart_item.save
+    end
+
+    redirect_to cart_items_path
   end
 
   def update
@@ -13,4 +25,9 @@ class Public::CartItemsController < ApplicationController
 
   def all_destroy
   end
+
+  private
+    def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+    end
 end
