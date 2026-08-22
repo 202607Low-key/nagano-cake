06e51880a9402bfd50da5902a6dda564ac629a41
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-08-22 02:46:12 +0000

    新規登録レイアウト、管理者側のfirst_nameとlast_nameの入れ替え

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,4 @@
-class Public::CartItemsController < ApplicationController
+class Public::CartItemsController < Public::ApplicationController
   def index
   end
 
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,4 @@
-class Public::ItemsController < ApplicationController
+class Public::ItemsController < Public::ApplicationController
   allow_unauthenticated_access only: [:index, :show]
   def index
     @items = Item.includes(:genre).page(params[:page]).where(is_active: true)
