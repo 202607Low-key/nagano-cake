d00852bc6d5b72f47bb207513d8faf9fa305176e
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-08-25 10:07:26 +0000

    注文入力画面完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,26 +12,37 @@ module Public
     end
 
     def confirm
+      if request.post?
+        delivery_info = build_delivery_info
+        if delivery_info.nil?
+          redirect_to new_order_path, alert: "お届け先情報が正しくありません"
+          return
+        end
+
+        session[:order_info] = {
+          payment_method: params[:order][:payment_method],
+          postal_code: delivery_info[:postal_code],
+          address: delivery_info[:address],
+          name: delivery_info[:name]
+        }
+
+        redirect_to confirm_orders_path
+        return
+      end
+
+      order_info = session[:order_info]
+      if order_info.blank?
+        redirect_to new_order_path, alert: "注文情報が見つかりません"
+        return
+      end
+
       @cart_items = current_customer.cart_items.includes(:item)
       if @cart_items.empty?
         redirect_to cart_items_path, alert: "カートが空です"
         return
       end
 
-      delivery_info = build_delivery_info
-      if delivery_info.nil?
-        redirect_to new_order_path, alert: "お届け先情報が正しくありません"
-        return
-      end
-
-      session[:order_info] = {
-        payment_method: params[:order][:payment_method],
-        postal_code: delovery_info[:postal_code],
-        address: delivery_info[:address],
-        name: delivery_info[:name]
-      }
-
-      @order = Order.new(session[:order_info])
+      @order = Order.new(order_info)
       @shipping_cost = 800
       @total_price = @cart_items.sum(&:subtotal)
       @total_payment = @total_price + @shipping_cost
@@ -111,7 +122,7 @@ module Public
         return nil unless selected
         { postal_code: selected.postal_code, address: selected.address, name: selected.name }
       when "new_address"
-        postal_code = parmas[:order][:postal_code]
+        postal_code = params[:order][:postal_code]
         address = params[:order][:address]
         name = params[:order][:name]
         return nil if postal_code.blank? || address.blank? || name.blank?
